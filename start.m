2a9d49d446d8469b5514a2ac2bf480d6a6bac468
thanhqtran <tranquangthanh.ftu@gmail.com>
2026-01-08 11:50:14 +0700

    add startup code for 2021

--- start.m
+++ start.m
@@ -5,11 +5,17 @@
 % Get the directory of this script
 thisDir = fileparts(mfilename('fullpath'));
 
-% Add relative paths
-addpath(fullfile(thisDir, 'IRIS_Tbx_20181028'));
+%% ===========================================
+%% Change startup command depending on version
+%% ===========================================
 
-% Start IRIS
+% Run startup for 2018 version
+addpath(fullfile(thisDir, 'IRIS_Tbx_20181028'));
 irisstartup;
+
+% Run startup for 2021 version
+addpath(fullfile(thisDir, 'IRIS-Toolbox-Legacy-20211206'));
+iris.startup;
 
 % Clean up
 clear thisDir;
